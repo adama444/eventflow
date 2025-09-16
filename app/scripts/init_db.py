a8c4f8fee17b4f9fc6ca797243898706e0f8d092
Adama SAMAKE <as91404togo@gmail.com>
2025-09-16 21:16:30 +0000

    fix: change memory database tables initialization method using asyncio.run()

--- app/scripts/init_db.py
+++ app/scripts/init_db.py
@@ -1,9 +1,19 @@
+import asyncio
+
+from app.agents.agent import init_chatbot_app
 from app.core.database import init_db
 from app.schemas.user import User
-from app.agents.agent import memory
 
-print("Creating tables...")
-init_db()
-print("Tables created")
-memory.setup()
-print("LangGraph memory tables initialized")
+
+async def main():
+    print("Creating tables...")
+    init_db()
+    print("Tables created")
+
+    chatbot_app = await init_chatbot_app()
+    # This will initialize memory and setup its tables
+    await chatbot_app.checkpointer.setup()
+    print("LangGraph memory tables initialized")
+
+
+asyncio.run(main())
